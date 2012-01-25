c7df0f529c65b7d51c41c85e3b8671a461f32c56
weisseruebe <a.rettig@gmx.de>
2012-01-25 13:37:25 +0100

    Dubble buffering

--- arduino/iPov2/iPov2.ino
+++ arduino/iPov2/iPov2.ino
@@ -10,7 +10,8 @@
 
 #include "debounce.h"
 #include "face.h"
-//#include "flup.h"
+#include "flup.h"
+
 
 #define NumLED 16
 #define Rows 64
@@ -28,18 +29,35 @@ long lstDebounceTime = 0;     // the last time the output pin was toggled
 long dbounceDelay = 10000;    // the debounce time; increase if the output flickers
 int inByte = 0;
 int offset = 0;
-int timeLineIndex = 0;
+float oneRow = 100;
 
 SerialPort<0, 150, 0> NewSerial;
 
 int receiveCounter = 0;
 int dispBuffer[Rows*2];
+int dispBuffer2[Rows*2];
 
+boolean dispToggle = true;
+boolean newData = false;
 
 /* Mapping from LED-No to Pin */
 int LEDs[] = {
   18,3,4,5,6,7,8,9,17,16,15,14,13,12,11,10
 };
+
+void setup() {
+  NewSerial.begin(115200);
+  for (int i = 0; i< 18; i++){
+    pinMode(i, OUTPUT); 
+  }
+  for (int i=0;i<Rows*2;i++){
+    dispBuffer[i]  = face[i];
+    dispBuffer2[i] = flup[i];
+  }
+  attachInterrupt(0, isr, RISING);
+  NewSerial.println("setup");
+}
+
 
 void setLED(int n, int state){
   if (n<16){
@@ -53,20 +71,9 @@ void setLeds(int bits, int group){
   }
 }
 
-void setup() {
-  NewSerial.begin(115200);
-  for (int i = 0; i< 18; i++){
-    pinMode(i, OUTPUT); 
-  }
-  for (int i=0;i<Rows*2;i++){
-    dispBuffer[i] = face[i];
-  }
-  attachInterrupt(0, on, RISING);
-  NewSerial.println("setup");
-}
 
-void receiveImage(){
-  //if (NewSerial.available()){
+void receiveData(){
+
   while(NewSerial.available())  {
     int inByte = NewSerial.read();
     /* Ready to receive */
@@ -92,7 +99,15 @@ void receiveImage(){
       /* Receive data */
       switch(receiveState){
       case  RECEIVE_IMAGE:
-        dispBuffer[127-receiveCounter] = inByte; // Drehen!
+        if (dispToggle){
+          dispBuffer2[127-receiveCounter] = inByte;
+        } 
+        else {
+          dispBuffer[127-receiveCounter] = inByte;
+        }
+        if (receiveCounter == 1) {
+          newData = true;
+        } //receiveCounter wird danach 0
         break;
       case RECEIVE_OFFSET:
         offset = inByte;
@@ -105,27 +120,37 @@ void receiveImage(){
   }
 }
 
-void on()
+void isr()
 {
   if (micros() -lstDebounceTime > dbounceDelay){
     NewSerial.print("l");
+    //Toggle the buffers if newData arrived
+    if (newData){
+      newData = false;
+      dispToggle = !dispToggle; 
+    }
+    //Calculate the time for one turn
     interval = micros()-lstDebounceTime;
+    //Save the last trigger time
     lstDebounceTime = micros();
+    oneRow = interval / Rows;
   }
 }
 
 void loop() {
-  receiveImage();
+  receiveData();
 
-  float oneRow = interval / Rows;
   int i = (micros() - lstDebounceTime) / oneRow;
   int index = (i % Rows * 2 + offset*2) % (Rows*2);
-  setLeds(dispBuffer[index],0);
-  setLeds(dispBuffer[index+1],1);
-
+  if (dispToggle){
+    setLeds(dispBuffer[index],0);
+    setLeds(dispBuffer[index+1],1);
+           //NewSerial.println("B1");
+  } 
+  else {
+    setLeds(dispBuffer2[index],0);
+    setLeds(dispBuffer2[index+1],1);
+           //NewSerial.println("B2");
+  }
 }
-
-
-
-
 
